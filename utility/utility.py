424a9dfce67a0c16ee33fa92d4ce09b2d9ea7888
clairepeng <zdimple0808@gmail.com>
2020-05-19 23:29:52 +0800

    added a parameter for replay()

--- utility/utility.py
+++ utility/utility.py
@@ -5,21 +5,34 @@ Created on Fri Apr 24 23:02:38 2020
 
 @author: clairepeng
 """
+import os.path
+import json
 
-def replay():
-    
+
+def get_project_root(path=__file__):
+    directory = os.path.dirname(path)
+
+    while directory != '/':
+        p = os.path.join(directory, 'requirements.txt')
+        if os.path.isfile(p):
+            return directory
+        else:
+            directory = os.path.dirname(directory)
+    return None
+
+
+def replay(string):
+
     while True:
-        
-        replay = input('Are you going to replay? y or n: ').lower()[0]
-        
+
+        replay = input(f'Are you going to {string}? y or n: ').lower()[0]
+
         if replay != 'y' and replay != 'n':
             continue
-        
+
         elif replay == 'y':
             return True
-        
+
         else:
             print('Goodbye!')
             return False
-
-
